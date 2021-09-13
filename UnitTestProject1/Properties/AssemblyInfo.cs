33c76e4f10308cec178f1438f199373208c836c7
ismailafsinbaran <89981279+ismailafsinbaran@users.noreply.github.com>
2021-09-13 14:04:24 +0300

    Assert çalışmaları
    Assert çalışmaları

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d38ea932-3ec5-4930-845e-9b38f5016fab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ClassLibraryPractice
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int result = ClassLibrary1.Class1.Addition(10, 5);
+            Assert.AreEqual(result, 15);
+        }
+    }
+}
